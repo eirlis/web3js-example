bd376cd52e9d115d2e195827189346db0bd4e661
lderimova <lderimova@s-pro.io>
2017-06-24 16:33:10 +0300

    Solidity contract parsed to json.

--- src/Reports.js
+++ src/Reports.js
@@ -1,12 +1,16 @@
+import Web3 from 'web3';
+import fs from 'fs';
+import jsonik from './sol.json';
 
 //TODO implement contract logic
-class Reports{
+class Reports {
 
     constructor(){
+        var web3 = new Web3();
+        web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+        alert(jsonik.value);
 
     }
-
-
 
 }
 
