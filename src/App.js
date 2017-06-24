ef72f90dfbe8ee468573467e6c49f16f634f44bd
dmi3coder <dmi3coder@gmail.com>
2017-06-24 15:12:04 +0300

    removed Apps state and props

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import './App.css';
 
 class App extends Component {
     constructor(props){
-       super(props)
-        this.state = {
-           goven:123,
-            parasha:[1,5,7]
-        };
-
-        setInterval(()=>{
-          this.setState({
-              goven: ++this.state.goven
-          })
-        },1000)
-
+       super(props);
     }
 
   render() {
